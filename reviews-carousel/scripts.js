657c4184586762fc49cdcf902ad23c6a07c2b3bb
Ivan Kok <ivanjayk@gmail.com>
2021-09-25 21:27:43 -0400

    Redo reviews-carousel project

--- reviews-carousel/scripts.js
+++ reviews-carousel/scripts.js
@@ -1,41 +1,46 @@
 import reviews from "./reviews.js";
 
-const leftBtn = document.getElementById("left-btn");
-const rightBtn = document.getElementById("right-btn");
-const randomBtn = document.getElementById("random-btn");
-const profileImg = document.getElementById("profile-img");
-const profileName = document.getElementById("name");
-const jobTitle = document.getElementById("job-title");
-const description = document.getElementById("description");
-
+const card = document.querySelector(".card");
+const [leftBtn, rightBtn] = card.querySelectorAll("i");
+const randBtn = card.querySelector("button");
 let currIndex = 0;
+
+const renderCard = () => {
+  const curr = reviews[currIndex];
+  const profileImg = card.querySelector("img");
+  const name = card.querySelector("h3");
+  const jobTitle = card.querySelector("h4");
+  const description = card.querySelector("p");
+
+  profileImg.src = curr.img;
+  profileImg.alt = `${curr.name}'s profile'`;
+  name.textContent = curr.name;
+  jobTitle.textContent = curr.job;
+  description.textContent = curr.text;
+};
 
 leftBtn.addEventListener("click", () => {
   currIndex -= 1;
-  if (currIndex < 0) {
-    currIndex = reviews.length - 1; // Wrap around
-  }
-  renderReview(currIndex);
+  if (currIndex < 0) currIndex = reviews.length - 1;
+
+  renderCard();
 });
 
 rightBtn.addEventListener("click", () => {
   currIndex += 1;
-  if (currIndex === reviews.length) {
-    currIndex = 0; // Wrap around
-  }
-  renderReview(currIndex);
+  if (currIndex == reviews.length) currIndex = 0;
+
+  renderCard();
 });
 
-randomBtn.addEventListener("click", () => {
-  currIndex = Math.floor(Math.random() * reviews.length);
-  renderReview(currIndex);
+randBtn.addEventListener("click", () => {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  let randIndex = Math.floor(Math.random() * reviews.length);
+  while (randIndex === currIndex)
+    randIndex = Math.floor(Math.random() * reviews.length);
+  console.log(randIndex);
+  currIndex = randIndex;
+  renderCard();
 });
 
-const renderReview = (index) => {
-  profileImg.src = reviews[index].img;
-  profileName.textContent = reviews[index].name;
-  jobTitle.textContent = reviews[index].job;
-  description.textContent = reviews[index].text;
-};
-
-renderReview(currIndex);
+renderCard();
